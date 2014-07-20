1ee608dfd0b138393f2f50fa92cbab10d7408cd6
bootcoder <bootcoder@gmail.com>
2014-07-20 13:48:04 -0700

    comments created routed and partialed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
 
 	validates_presence_of :body, :title
 
-	
+	has_many :comments
+		
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
-  resources :posts
+
+  resources :posts do
+    resources :comments, only: :create
+  end
 
   resources :users
 
