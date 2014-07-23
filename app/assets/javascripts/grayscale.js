829cb9fa400e0facfebdfc6a07211251131aee9a
bootcoder <bootcoder@gmail.com>
2014-07-22 20:35:53 -0700

    changed styling significantly - still playing with colors

--- app/assets/javascripts/grayscale.js
+++ app/assets/javascripts/grayscale.js
@@ -26,7 +26,7 @@ $(function() {
 
 // Google Maps Scripts
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
+// google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
     // Basic options for a simple Google Map
