4131cda08aa79aaa44b7a1acf642c62d4201fba4
bootcoder <bootcoder@gmail.com>
2014-07-28 20:21:49 -0700

    spacer divs not rotating -- not pretty but functional

--- app/assets/javascripts/single-page.js
+++ app/assets/javascripts/single-page.js
@@ -118,7 +118,7 @@ var scrollDown = function() {
 var spacerRotate = function() {
 	console.log("spacerRotate 200");
 
-	var displayTime = 5000,
+	var displayTime = 10000,
 			transitionTime = 2000;
 
 	var currentIndex = 0;
@@ -126,50 +126,15 @@ var spacerRotate = function() {
 	var $slides = $('#single-display-spacers section');
 	console.log($slides);
 	function animateBG() {
+		$('#index-spacer'+currentIndex).hide();
 		console.log("inside-animateBG");
 		currentIndex = (currentIndex < 3) ? currentIndex + 1 : 0;
+		$('#index-spacer'+currentIndex).show();
 		setTimeout(function(){
 			console.log("inside-timeout");
 			console.log(currentIndex);
-			$('#index-spacer'+currentIndex).fadeIn(transitionTime);
-			
-			// $slides.css('z-index', 1);
-			// $slides.eq(currentIndex).css('z-index', 2).fadeIn(transitionTime, function(){
-			// 	$slides.not(this).hide();
-				animateBG();
-
-			
+			animateBG();
 		},	displayTime);
 	}
-
 	animateBG();
 };
-
-// var spacerRotate = function() {
-// 	var displayTime = 10000,
-// 			transitionTime = 1500;
-
-// 	var currentIndex = 0;
-
-// 	console.log("inside spacerRotate");
-	
-// 		for(var i = 0; i < 5; i += 0) {
-// 			console.log("inside for loop");
-// 			setTimeout(function(){
-// 				console.log("inside setTimeout");
-// 				$(".index-spacer"+i).fadeOut(1000);
-// 				console.log(i);
-// 				i += 1;
-// 				console.log(i);
-// 				$(".index-spacer"+i).fadeIn(transitionTime);
-// 			},3000);
-// 	}
-// };
-
-
-
-
-
-
-
-
