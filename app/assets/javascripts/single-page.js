619372565457d9219b2d86eecf2e37fb52ad60d1
bootcoder <bootcoder@gmail.com>
2014-07-28 20:06:11 -0700

    displaying spacer divs on setTimout

--- app/assets/javascripts/single-page.js
+++ app/assets/javascripts/single-page.js
@@ -116,32 +116,55 @@ var scrollDown = function() {
 ////////////////////////////////////
 
 var spacerRotate = function() {
+	console.log("spacerRotate 200");
 
 	var displayTime = 5000,
 			transitionTime = 2000;
 
 	var currentIndex = 0;
 
-	var $slides = $('#single-display-spacers div');
-
+	var $slides = $('#single-display-spacers section');
+	console.log($slides);
 	function animateBG() {
+		console.log("inside-animateBG");
 		currentIndex = (currentIndex < 3) ? currentIndex + 1 : 0;
 		setTimeout(function(){
-			$slides.css('z-index', 1);
-			$slides.eq(currentIndex).css('z-index', 2).fadeIn(transitionTime, function(){
-				$slides.not(this).hide();
+			console.log("inside-timeout");
+			console.log(currentIndex);
+			$('#index-spacer'+currentIndex).fadeIn(transitionTime);
+			
+			// $slides.css('z-index', 1);
+			// $slides.eq(currentIndex).css('z-index', 2).fadeIn(transitionTime, function(){
+			// 	$slides.not(this).hide();
 				animateBG();
 
-			});
+			
 		},	displayTime);
 	}
 
 	animateBG();
 };
 
+// var spacerRotate = function() {
+// 	var displayTime = 10000,
+// 			transitionTime = 1500;
 
+// 	var currentIndex = 0;
 
-
+// 	console.log("inside spacerRotate");
+	
+// 		for(var i = 0; i < 5; i += 0) {
+// 			console.log("inside for loop");
+// 			setTimeout(function(){
+// 				console.log("inside setTimeout");
+// 				$(".index-spacer"+i).fadeOut(1000);
+// 				console.log(i);
+// 				i += 1;
+// 				console.log(i);
+// 				$(".index-spacer"+i).fadeIn(transitionTime);
+// 			},3000);
+// 	}
+// };
 
 
 
