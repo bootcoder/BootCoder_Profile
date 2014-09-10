c1850d6fa9226f39a8efde4bf0f67f4543dcac57
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-09-10 16:18:09 -0700

    moved hard coded project factory to factories folder

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,15 +2,6 @@ require "spec_helper"
 
 describe Project do
   context "Validations" do
-    before(:each) do
-      @project = Project.new
-      @project.project_description = "test project"
-      @project.project_url = "www.test-project.com"
-      @project.project_source_url = "www.githubz.com"
-      @project.project_img = "Hunterbuttz.png"
-      @project.project_feature_lang = "Ruby"
-      @project.project_state = "pre-alpha"
-    end
     it { should validate_presence_of(:project_description)}
     it { should validate_presence_of(:project_url) }
     it { should validate_presence_of(:project_source_url) }
