a95197ac9b2e0bcb177476770bbbbc82efa33250
Hunter Chapman <bootcoder@gmail.com>
2014-10-06 15:15:32 -0700

    add homebase- remove validations from projects lang & status -- add if look for non sourse or url projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Project.create(project_name: "TechLX (CLOSED SOURCE)",
+Project.create(project_name: "TechLX",
 							 project_description: "Landing page for Non-Profit Educational Outreach. Application currently under development. Contributing heavily to the front-end and testing with Rspec. Links available upon completion.", 
 							 project_url: "http://www.techlx.com/", 
 							 project_source_url: "closed", 
 							 project_img: "techlx-grab2.png", 
 							 project_feature_lang: "Rails", 
 							 project_state: "Active")
-
-Project.create(project_name: "DBCode",
-							 project_description: "A DBC project written in Sinatra. The goal is to create an educational resource that can help students get un-blocked quickly. Focus on CRUD and API integration.", 
-							 project_url: "http://dbcode.herokuapp.com", 
-							 project_source_url: "https://github.com/bootcoder/DBCode", 
-							 project_img: "dbcode.jpg", 
-							 project_feature_lang: "Sinatra", 
-							 project_state: "Beta")
 
 Project.create(project_name: "MetaGame",
 							 project_description: "An application to bring gamers together on a local level. This group project came together in a single week @ DBC. Our team of 5 put in over 100 hours each. GROUP HIGHFIVES!", 
@@ -30,6 +22,14 @@ Project.create(project_name: "MetaGame",
 							 project_img: "mg-games-grab.jpg", 
 							 project_feature_lang: "RoR", 
 							 project_state: "Cold Beta")
+
+Project.create(project_name: "DBCode",
+							 project_description: "SOLO DBC project started in June. The goal is to create an educational resource that can help students get un-blocked quickly. Focus on CRUD and API integration. Future plans include folding in modules from other DBC related apps to create a student resource hub.", 
+							 project_url: "http://dbcode.herokuapp.com", 
+							 project_source_url: "https://github.com/bootcoder/DBCode", 
+							 project_img: "dbcode.jpg", 
+							 project_feature_lang: "Sinatra", 
+							 project_state: "Beta")
 
 Project.create(project_name: "DoppelTwit",
 							 project_description: "Early attempt at a Twitter Clone. Team of 4 constructed this application in 24 hours. Contributions include a M2M self referential schema and an excellent intro to GIT workflow. ", 
@@ -40,7 +40,13 @@ Project.create(project_name: "DoppelTwit",
 							 project_state: "Cold Alpha")
 
 
-
+Project.create(project_name: "HomeBase",
+							 project_description: "Application to allow communities of renters to quickly share and digest lastest listings. Hackathon app created in 12 hours. Uses Kimono API", 
+							 project_url: "none", 
+							 project_source_url: "https://github.com/dstavis/homebase-hackathon", 
+							 project_img: "homebase-grab.png", 
+							 project_feature_lang: "Sinatra", 
+							 project_state: "Cold Alpha")
 
 # Project.create(project_name: "",
 # 							 project_description: "", 
