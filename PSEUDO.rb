1fffe9b08065f85343697e4d8380ccf5a961e6db
Hunter Chapman <bootcoder@gmail.com>
2014-10-23 01:05:22 -0700

    add pseudo file, update readme

---
+++ PSEUDO.rb
@@ -0,0 +1,4 @@
+# New spacer content
+# 
+# I’m looking for a terrific environment to continue my journey growing as a developer. A place I can find a mentor or just a great pairing partner, a place that’s fun, witty, smart and a place that is as driven as I am to grow outside the bounds of where we are today. 
+
