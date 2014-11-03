aa85e96acb24486089e437fbf22c3390b652e535
Hunter Chapman <bootcoder@gmail.com>
2014-11-02 19:13:56 -0800

    clean mailer methods

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,10 @@
+require 'dotenv'
+
 class ContactMailer < ActionMailer::Base
   default from: "bootcoder@gmail.com"
   default to: "bootcoder@gmail.com"
 
   def send_email(user_info)
-  	p "^" * 50
-  	p ENV['SENDGRID_USERNAME']
-  	p "^" * 50
   	@body = user_info.body
   	mail(email: user_info.email, subject: "Profile MSG from #{user_info.name}")
   end
--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => Rails.application.secrets.sendgrid_username,
+  :password       => Rails.application.secrets.sendgrid_password,
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
