a71ec763faefbf40bb889cc1797b1723072071bd
Hunter Chapman <bootcoder@gmail.com>
2014-11-02 19:33:58 -0800

    mailer complete - updated view template for email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,7 @@ class ContactMailer < ActionMailer::Base
   default to: "bootcoder@gmail.com"
 
   def send_email(user_info)
-  	@body = user_info.body
+  	@body = user_info
   	mail(email: user_info.email, subject: "Profile MSG from #{user_info.name}")
   end
 end
