abf311400e0f921744c1b1902b3ef40187e32362
Hunter Chapman <bootcoder@gmail.com>
2014-11-02 17:32:18 -0800

    add basic info to mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,3 +1,9 @@
 class ContactMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "someone@bootcoder.com"
+  default to: "bootcoder@gmail.com"
+
+  def send_mail(user_info)
+  	@body = user_info
+  	mail(email: user_info.email, subject: "Profile MSG from #{user_info.name}")
+  end
 end
