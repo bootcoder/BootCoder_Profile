f2bfb6ce89efdc3650b227bd46bde3b0d7a7bb27
BootCoder <bootcoder@gmail.com>
2018-03-25 11:03:11 -0700

    update mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,9 @@
-require 'dotenv'
-
 class ContactMailer < ActionMailer::Base
   default from: "bootcoder@gmail.com"
   default to: "bootcoder@gmail.com"
 
   def send_email(user_info)
   	@body = user_info
-
   	mail(email: user_info.email, subject: "Profile MSG from #{user_info.name}")
   end
 end
