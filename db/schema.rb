8e6d10e40c1cf4cf58769610dd374565d0469fbf
Hunter T. Chapman <hchapman@grio.com>
2025-06-06 08:44:09 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,47 +1,44 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031093240) do
-
+ActiveRecord::Schema[8.0].define(version: 2014_10_31_093240) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "post_id"
-    t.text     "body"
+    t.integer "post_id"
+    t.text "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
-
   create_table "posts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.text     "body"
+    t.string "name"
+    t.string "email"
+    t.text "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string   "project_name"
-    t.text     "project_description"
-    t.text     "project_stack"
-    t.string   "project_url"
-    t.string   "project_source_url"
-    t.string   "project_img"
-    t.string   "project_feature_lang"
-    t.string   "project_state"
+    t.string "project_name"
+    t.text "project_description"
+    t.text "project_stack"
+    t.string "project_url"
+    t.string "project_source_url"
+    t.string "project_img"
+    t.string "project_feature_lang"
+    t.string "project_state"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -53,12 +50,11 @@ ActiveRecord::Schema.define(version: 20141031093240) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password"
-    t.string   "user_avatar"
-    t.boolean  "user_admin"
+    t.string "username"
+    t.string "password"
+    t.string "user_avatar"
+    t.boolean "user_admin"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 end
