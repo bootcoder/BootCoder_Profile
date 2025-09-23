a60ce131ee5ce7a26823fe150d069165cb859f87
Hunter T. Chapman <hchapman@grio.com>
2025-09-23 12:46:41 -0400

    mv cache control to application_controller

--- config/initializers/cache_control.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Runtime do |env|
-  status, headers, response = @app.call(env)
-
-  # Only set if origin didn't specify already
-  headers['Cache-Control'] ||= 'public, max-age=300'
-
-  [status, headers, response]
-end
