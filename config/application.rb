2a193ff3d35b2013b16267cf0d0b01042999c35e
Hunter T. Chapman <hchapman@grio.com>
2025-09-27 15:00:26 -0400

    spk patch ruby_llm

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+RubyLLM.configure do |config|
+  config.use_new_acts_as = true
+end
+
 module BootCoder
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
