8c0dac92973ce148b0caabc729f59e4031082b84
Hunter T. Chapman <hchapman@grio.com>
2025-10-05 01:01:04 -0400

    play with js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,30 +3,30 @@ $(document).ready(function(){
   var owl = $("#owl-slider");
   var gallery = $("#gallery-slider")
 
-  // debugger;
-  owl.owlCarousel({
-      items : 2,
-      itemsDesktop : [1199,2],
-      itemsDesktopSmall : [900,1], // betweem 900px and 601px
-      itemsTablet: [600,1], //1 item between 600 and 0
-      itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-      autoPlay: true,
-      navigation: true,
-      paginationSpeed: 800,
-      slideSpeed: 200,
-      rewindSpeed: 4000,
-      stopOnHover: true
-  });
+  // // debugger;
+  // owl.owlCarousel({
+  //     items : 2,
+  //     itemsDesktop : [1199,2],
+  //     itemsDesktopSmall : [900,1], // betweem 900px and 601px
+  //     itemsTablet: [600,1], //1 item between 600 and 0
+  //     itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+  //     autoPlay: true,
+  //     navigation: true,
+  //     paginationSpeed: 800,
+  //     slideSpeed: 200,
+  //     rewindSpeed: 4000,
+  //     stopOnHover: true
+  // });
 
-  gallery.owlCarousel({
-      transitionStyle: "goDown",
-      items : 2,
-      itemsDesktop : [1199,2],
-      itemsDesktopSmall : [900,1], // betweem 900px and 601px
-      itemsTablet: [600,1], //1 item between 600 and 0
-      itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-      autoPlay: true
-  });
+  // gallery.owlCarousel({
+  //     transitionStyle: "goDown",
+  //     items : 2,
+  //     itemsDesktop : [1199,2],
+  //     itemsDesktopSmall : [900,1], // betweem 900px and 601px
+  //     itemsTablet: [600,1], //1 item between 600 and 0
+  //     itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+  //     autoPlay: true
+  // });
 
   $('.popup').on('click', function(event){
     console.log("CLICKDD ME!");
@@ -34,15 +34,15 @@ $(document).ready(function(){
   })
 
 
-  if ( $(window).width() < 1023 ) {
-    $('.bg-space').removeClass('bg-space black')
-    $('.bg-space').addClass('gray')
-    $('.box-team').removeClass('wow')
-    $('.service-box').removeClass('wow')
-    $('.mobile-js').removeClass('max-400')
-    $('.mobile-js').removeClass('max-500')
-    $('.mobile-js').addClass('max-90P')
+  // if ( $(window).width() < 1023 ) {
+  //   $('.bg-space').removeClass('bg-space black')
+  //   $('.bg-space').addClass('gray')
+  //   $('.box-team').removeClass('wow')
+  //   $('.service-box').removeClass('wow')
+  //   $('.mobile-js').removeClass('max-400')
+  //   $('.mobile-js').removeClass('max-500')
+  //   $('.mobile-js').addClass('max-90P')
 
-  }
+  // }
 
 });
