387079820e23aaa306ea819cd504621ad14c900f
Hunter T. Chapman <hchapman@grio.com>
2025-10-09 17:05:57 -0400

    order techs alpha

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-    @techs = Tech.all
+    @techs = Tech.all.order(:title)
   end
 
   # GET /users/1
