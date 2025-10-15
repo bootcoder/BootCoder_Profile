6c64ab54806d07af2b222617786016bf2aae249c
Hunter T. Chapman <hchapman@grio.com>
2025-10-15 11:59:16 -0400

    migration: add source string to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_08_191608) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_15_155840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_08_191608) do
     t.boolean "user_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source"
   end
 
 end
