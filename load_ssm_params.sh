e85487a782027d2df86aebd94dbfaccea12713ab
Hunter T. Chapman <hchapman@grio.com>
2025-10-16 11:53:42 -0400

    add load_ssm_params script

---
+++ load_ssm_params.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+SSM_PATH="/prod/bootcore/"
+ENV_DIR="/home/ec2-user/BootCoder_Profile"
+TMP_FILE="$ENV_DIR/.env.prod.tmp"
+FINAL_FILE="$ENV_DIR/.env.production"
+
+echo "AWS SSM: Pulling Params from $SSM_PATH"
+
+# clear tmp file first
+> "$TMP_FILE"
+
+aws ssm get-parameters-by-path \
+  --path "$SSM_PATH" \
+  --with-decryption \
+  --recursive \
+  --query "Parameters[*].{Name:Name,Value:Value}" \
+  --output text | while read NAME VALUE; do
+
+  VAR_NAME=$(echo "$NAME" | awk -F'/' '{print toupper($NF)}')
+  echo "$VAR_NAME=$VALUE" >> "$TMP_FILE"
+
+done
+
+# Deduplicate into final
+sort -u "$TMP_FILE" > "$FINAL_FILE"
