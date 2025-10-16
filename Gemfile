ccf9a31d9e558c14940d449001cb937d43674438
Hunter T. Chapman <hchapman@grio.com>
2025-10-16 19:41:16 -0400

    swap uglifier -> terser

--- Gemfile
+++ Gemfile
@@ -9,8 +9,8 @@ gem "rails", "~> 7.1.3"
 gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails'
-# Use Uglifier as compressor for JavaScript assets
-gem 'uglifier'
+# Use terser as compressor for JavaScript assets
+gem 'terser' # terser-ruby
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,32 +1,5 @@
 $(document).ready(function(){
   console.log("Document Ready");
-  // var owl = $("#owl-slider");
-  // var gallery = $("#gallery-slider")
-
-  // // debugger;
-  // owl.owlCarousel({
-  //     items : 2,
-  //     itemsDesktop : [1199,2],
-  //     itemsDesktopSmall : [900,1], // betweem 900px and 601px
-  //     itemsTablet: [600,1], //1 item between 600 and 0
-  //     itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-  //     autoPlay: true,
-  //     navigation: true,
-  //     paginationSpeed: 800,
-  //     slideSpeed: 200,
-  //     rewindSpeed: 4000,
-  //     stopOnHover: true
-  // });
-
-  // gallery.owlCarousel({
-  //     transitionStyle: "goDown",
-  //     items : 2,
-  //     itemsDesktop : [1199,2],
-  //     itemsDesktopSmall : [900,1], // betweem 900px and 601px
-  //     itemsTablet: [600,1], //1 item between 600 and 0
-  //     itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-  //     autoPlay: true
-  // });
 
   $('.popup').on('click', function(event){
     console.log("CLICKDD ME!");
