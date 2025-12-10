8fa493e7324ecaad109b6168fbbc8e85c5c60b20
Hunter T. Chapman <hchapman@grio.com>
2025-12-10 14:18:04 -0500

    first pass funny caro

--- app/helpers/giggle_helper.rb
+++ app/helpers/giggle_helper.rb
@@ -9,4 +9,16 @@ module GiggleHelper
               { alt: File.basename(logical, '.*'),
                 class: 'image-meme image-responsive max-500 pad-5P' }.merge(options)
   end
+
+  def giggle_images(**options)
+    root     = Rails.root.join('app','assets','images')
+    glob     = root.join('giggles','**','*.{png,jpg,jpeg,gif,webp,svg}')
+    files    = Dir.glob(glob.to_s, File::FNM_CASEFOLD)
+    logical  = files.sample&.sub(root.to_s + '/', '') || 'giggles/giggle_0.png'
+    result   = []
+    files.each do |file|
+      result << files.sample&.sub(root.to_s + '/', '') || 'giggles/giggle_0.png'
+    end
+    result
+  end
 end
